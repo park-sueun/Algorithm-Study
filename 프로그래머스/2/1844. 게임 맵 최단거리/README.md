013309c9c3a71b57c5b76056f7e111be08f2a0ce
Sueun PARK <60188351+park-sueun@users.noreply.github.com>
2026-06-16 18:18:01 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.34 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
 
 ### 성능 요약
 
-메모리: 11.5 MB, 시간: 5.28 ms
+메모리: 11.3 MB, 시간: 6.34 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 06월 16일 15:43:41
+2026년 06월 16일 18:17:57
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,45 +1,47 @@
+# 문제정의 : (0, 0) -> (n-1, m-1) 까지 최단 거리 문제. 상하좌우 한칸 -> 1 가중치 X, 도착하지 못할 수 있음(-1)
+# 알고리즘 선택 : BFS -> 가까운 노드부터 탐색하므로 도착 지점에 처음 도달한 거리가 최단 거리입니다.
+# 시간복잡도 : 맵의 크기 O(N*M) 각 칸 방문 최대 1회
+# 질문: DFS가 안되는 이유 -> 한번 가면 돌아올 필요가 없지만 DFS는 돌아옴, 한번에 끝까지 갔다가 와서 비효율적임
+
 from collections import deque
 
 def solution(maps):
-    # 시작점(0,0) -> 도착점(n-1, m-1)까지의 최단 거리
-    # BFS를 활용
-    # 최단 거리에 DFS를 안쓰는 이유
-    # : 모든 경로를 다 탐색해야 함
-    
     n = len(maps)
     m = len(maps[0])
     
     directions = [
-        # 상하좌우
-        (0, -1), (0, 1), (-1, 0), (1, 0)
+        (0, -1), (0, 1), (-1, 0), (1,0)
     ]
     
-    q = deque() # 시작점
+    # BFS 큐에 무엇을 넣을 것인가 -> 현재 위치
+    
+    q = deque()
     q.append((0, 0))
     
     while q:
-        x, y = q.popleft()
+        # 이미 방문한 곳은 다시 방문하지 않는다.
+        # 벽은 이동할 수 없다.
+        # 격자를 벗어날 수 없다.
         
+        x, y = q.popleft()
         for dx, dy in directions:
             nx = x + dx
             ny = y + dy
             
-            # 범위를 벗어난 경우
             if not (0 <= nx < n and 0 <= ny < m):
                 continue
             
-            # 벽(0) 이거나 이미 거쳐간 경우(> 1)
-            if maps[nx][ny] != 1:
-                continue;
+            if maps[nx][ny] == 0:
+                continue
                 
+            if maps[nx][ny] != 1:
+                continue
+                
+            # 방문(현재 칸까지의 이동거리)
             maps[nx][ny] = maps[x][y] + 1
+            
             q.append((nx, ny))
             
-    # maps가 > 1이면 도착 아니면 도착 못함
     return maps[n-1][m-1] if maps[n-1][m-1] > 1 else -1
-                
-    
-    
-    
-    
-    
+        
+        
